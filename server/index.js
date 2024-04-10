314537216a6576c76e8f57497a0ed59716592fdd
Marco Bolaños <ingmarbol@gmail.com>
2024-04-10 18:06:20 -0500

    Finalizar todo

--- server/index.js
+++ server/index.js
@@ -68,6 +68,26 @@ app.put("/update", (req, res)=>{
     );
 });
 
+//METODO PARA ELIMINAR DATOS
+//En la ruta ponemos que se va a pasar un parámetro, en este caso
+//de nombre id
+app.delete("/delete/:id", (req, res)=>{
+    //Solo vamos a recibir el ID
+    const id = req.params.id //params especifica que es un parámetro
+
+
+    db.query('DELETE FROM empleado WHERE id=?', id,
+    (err, result)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.send(result)  //Se manda el result para que desde el frontend
+                              //puedan capturar esa información
+        }
+    }
+    );
+});
+
 app.listen(3001, ()=>{
     console.log("Corriendo en el puerto 3001")
 })
