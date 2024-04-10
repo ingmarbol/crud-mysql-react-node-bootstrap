6e88f9af251c54c8cbc8acbb73cd999b2aaaf3e4
Marco Bolaños <ingmarbol@gmail.com>
2024-04-09 20:34:52 -0500

    Mantenimiento Empleados con Creación y Listado

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2")
+const cors = require("cors")
+
+//Aquí se le dice a la aplicación que antes
+//de utilizar cualquier cosa use cors
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "empleados_crud"
+});
+
+//METODO PARA GUARDAR DATOS
+app.post("/create", (req, res)=>{
+    const nombre = req.body.nombre
+    const edad = req.body.edad
+    const pais = req.body.pais
+    const cargo = req.body.cargo
+    const anios = req.body.anios
+
+    db.query('INSERT INTO empleado(nombre, edad, pais, cargo, anios) VALUES(?,?,?,?,?)', [nombre, edad, pais, cargo, anios],
+    (err, result)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.send("Empleado registrado con éxito!!!")
+        }
+    }
+    );
+});
+
+//METODO PARA LISTAR DATOS
+app.get("/empleados", (req, res)=>{
+    db.query('SELECT * FROM empleado',
+    (err, result)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    }
+    );
+});
+
+app.listen(3001, ()=>{
+    console.log("Corriendo en el puerto 3001")
+})
+
+
